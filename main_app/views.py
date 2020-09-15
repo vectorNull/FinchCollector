bf782c2a54ab0468c46ad31146ded4668946431e
vectorNull <jose.rodriguez76@protonmail.com>
2020-09-15 14:09:48 -0700

    Remove hardcoded data

--- main_app/views.py
+++ main_app/views.py
@@ -1,19 +1,7 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from .models import Finch
 # Create your views here.
 
-class Finch:  # Note that parens are optional if not inheriting from another class
-  def __init__(self, name, color, description, age):
-    self.name = name
-    self.color = color
-    self.description = description
-    self.age = age
-
-finches = [
-  Finch('Will', 'red', 'gentle', 3),
-  Finch('Sammy', 'green', 'fiesty', 0),
-  Finch('Bob', 'blue', 'legless', 4)
-]
 
 def home(request):
     return HttpResponse('<h1>Hello /ᐠ｡‸｡ᐟ\ﾉ</h1>')
@@ -22,4 +10,9 @@ def about(request):
     return render(request, 'about.html')
 
 def finches_index(request):
-  return render(request, 'finches/index.html', { 'finches': finches })
+    finches = Finch.objects.all()
+    return render(request, 'finches/index.html', { 'finches': finches })
+
+def finch_detail(request, finch_id):
+  finch = Finch.objects.get(id=finch_id)
+  return render(request, 'finches/detail.html', { 'finch': finch })
