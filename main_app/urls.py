08a26acf046e392297043b79b1e7dd22a0c99d4e
vectorNull <jose.rodriguez76@protonmail.com>
2020-09-15 14:09:29 -0700

    Add details path

--- main_app/urls.py
+++ main_app/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('about/', views.about, name='about'),
-    path('finches/', views.finches_index, name='index')
+    path('finches/', views.finches_index, name='index'),
+    path('finches/<int:finch_id>/', views.finch_detail, name='detail'),
 ]
