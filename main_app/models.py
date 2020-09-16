70f5cc71928ef8753f94a5293b42d79ec2966006
vectorNull <jose.rodriguez76@protonmail.com>
2020-09-16 14:46:55 -0700

    Create Feeding class

--- main_app/models.py
+++ main_app/models.py
@@ -1,6 +1,13 @@
 from django.db import models
+from django.urls import reverse
 
 # Create your models here.
+MEALS = (
+    ('L', 'Lettuce'),
+    ('G', 'Grapes'),
+    ('C', 'Carrots')
+)
+
 class Finch(models.Model):
     name = models.CharField(max_length=100)
     color = models.CharField(max_length=100)
@@ -9,3 +16,20 @@ class Finch(models.Model):
 
     def __str__(self):
         return self.name
+    
+    def get_absolute_url(self):
+        return reverse('detail', kwargs={'finch_id': self.id})
+
+class Feeding(models.Model):
+    date = models.DateField('Feeding date')
+    meal = models.CharField(
+        max_length=1,
+        choices=MEALS,  #Allow for dropdowns
+        default=MEALS[0][0]
+        )
+    finch = models.ForeignKey(Finch, on_delete=models.CASCADE)
+    def __str__(self):
+        return f"{self.get_meal_display()} on {self.date}"
+    class Meta:
+        ordering = ['-date']    #sorts in descending order
+
