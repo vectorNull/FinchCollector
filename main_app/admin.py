1390418066beea10682b70af6f9cc0e6c624e5bd
vectorNull <jose.rodriguez76@protonmail.com>
2020-09-16 14:45:54 -0700

    Register Feeding model

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Finch
+from .models import Finch, Feeding
 
 admin.site.register(Finch)
+admin.site.register(Feeding)
