a30d838aecc03ec8ddb1a8922816a6d4dc908cae
vectorNull <jose.rodriguez76@protonmail.com>
2020-09-16 14:47:41 -0700

    Add feeding functions

--- main_app/views.py
+++ main_app/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from .models import Finch
+from .forms import FeedingForm
 # Create your views here.
 
 
@@ -14,5 +16,30 @@ def finches_index(request):
     return render(request, 'finches/index.html', { 'finches': finches })
 
 def finch_detail(request, finch_id):
-  finch = Finch.objects.get(id=finch_id)
-  return render(request, 'finches/detail.html', { 'finch': finch })
+    finch = Finch.objects.get(id=finch_id)
+    feeding_form = FeedingForm()
+    return render(request, 'finches/detail.html', {
+        'finch': finch, 'feeding_form': feeding_form
+    })
+
+def add_feeding(request, finch_id):
+  form = FeedingForm(request.POST)
+  if form.is_valid():
+      new_feeding = form.save(commit=False)
+      new_feeding.finch_id = finch_id
+      new_feeding.save()
+  return redirect('detail', finch_id=finch_id)
+
+class FinchCreate(CreateView):
+    model = Finch
+    fields = '__all__'
+
+class FinchUpdate(UpdateView):
+    model = Finch
+    fields = ['color', 'description', 'age']
+
+class FinchDelete(DeleteView):
+    model = Finch
+    success_url = '/finches/'
+
+
